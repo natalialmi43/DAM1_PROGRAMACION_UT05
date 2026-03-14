58e659785aa910c348779deb7d57280bd2e23a25
Natalia <natalia.marcos@educa.jcyl.es>
2026-03-14 23:34:28 +0100

    correcciones tienda

--- src/ejerciciosCasa/e03ListTiendaVideojuegos/Tienda.java
+++ src/ejerciciosCasa/e03ListTiendaVideojuegos/Tienda.java
@@ -23,6 +23,11 @@ public class Tienda {
         listadoVideojuegos.add(videojuego);
     }
 
+    //Se añade cliente
+    public void addCliente (Cliente cliente){
+        listadoClientes.add(cliente);
+    }
+
     // Mostrar todo el inventario.
     public ArrayList <Videojuego> getListadoVideojuegos(){
         return listadoVideojuegos;
