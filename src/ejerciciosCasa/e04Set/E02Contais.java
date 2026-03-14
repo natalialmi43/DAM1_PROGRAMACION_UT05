f160862314e3cf7032b76b29499d2af445a99227
Natalia <natalia.marcos@educa.jcyl.es>
2026-03-14 23:48:35 +0100

    WIP

---
+++ src/ejerciciosCasa/e04Set/E02Contais.java
@@ -0,0 +1,18 @@
+package ejerciciosCasa.e04Set;
+
+import java.util.HashSet;
+
+public class E02Contais {
+
+    public static void main(String[] args) {
+
+        HashSet <Integer> ids = new HashSet<>();
+
+        ids.add(1);
+        ids.add(2);
+        ids.add(3);
+        ids.add(4);
+        ids.add(5);
+
+    }
+}
