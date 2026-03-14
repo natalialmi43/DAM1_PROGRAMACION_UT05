5b4bb69f9405e192153e2c85d7f8bc4e67e25637
Natalia <natalia.marcos@educa.jcyl.es>
2026-03-14 13:04:33 +0100

    Se añade cliente

---
+++ src/ejerciciosCasa/e02ListVideoclub/Cliente.java
@@ -0,0 +1,37 @@
+package ejerciciosCasa.e02ListVideoclub;
+
+public class Cliente {
+
+    private int numeroCarnet;
+    private String nombre;
+
+    public Cliente(int numeroCarnet, String nombre) {
+        this.numeroCarnet = numeroCarnet;
+        this.nombre = nombre;
+    }
+
+    public void setNumeroCarnet(int numeroCarnet) {
+        if(numeroCarnet < 0){
+            this.numeroCarnet = 0000000;
+        } else {
+            this.numeroCarnet = numeroCarnet;
+        }
+    }
+
+    public int getNumeroCarnet() {
+        return numeroCarnet;
+    }
+
+    public void setNombre(String nombre) {
+        if(nombre==null || nombre.isEmpty()){
+            this.nombre = "defecto";
+        } else {
+            this.nombre = nombre;
+        }
+    }
+
+    public String toString(){
+
+        return "El cliente " + nombre + " con numero de carnet "+ numeroCarnet;
+    }
+}
