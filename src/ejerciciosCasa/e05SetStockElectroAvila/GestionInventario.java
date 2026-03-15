50cf082bf77d6ab9ef31e02ba2267858095d0cf6
Natalia <natalia.marcos@educa.jcyl.es>
2026-03-15 16:56:08 +0100

    Se realiza ejercicio Stock Electro Ávila

--- src/ejerciciosCasa/e05SetStockElectroAvila/GestionInventario.java
+++ src/ejerciciosCasa/e05SetStockElectroAvila/GestionInventario.java
@@ -1,3 +1,5 @@
+package ejerciciosCasa.e05SetStockElectroAvila;
+
 import java.util.*;
 
 public class GestionInventario {
@@ -5,7 +7,7 @@ public class GestionInventario {
         // PARTE HECHA: Datos de prueba con duplicados (mismo ID, distintos datos)
         List<Producto> entradaAlmacen = new ArrayList<>();
         entradaAlmacen.add(new Producto("LAV01", "Lavadora Bosch", 450.0, 10));
-        entradaAlmacen.add(new Producto("TV02",  "TV Samsung 60\"", 800.0, 5));
+        entradaAlmacen.add(new Producto("TV02",  "TV Samsung 60", 800.0, 5));
         entradaAlmacen.add(new Producto("LAV01", "Lavadora Bosch XL", 480.0, 2)); // Duplicado ID
         entradaAlmacen.add(new Producto("PC03",  "PC Gaming Nitro", 1200.0, 3));
         entradaAlmacen.add(new Producto("TV02",  "TV Samsung OLED", 850.0, 1));  // Duplicado ID
@@ -15,18 +17,35 @@ public class GestionInventario {
 
         // TODO: TAREA 1 - Eliminar duplicados usando un Set
         // Pista: El Set usará vuestro método equals/hashCode[cite: 2696].
-        Set<Producto> productosUnicos = null; // Completar
+        Set<Producto> productosUnicos = new HashSet<>(entradaAlmacen);
+
 
         // TODO: TAREA 2 - Volver a pasar a una List para poder ordenar
-        List<Producto> listaLimpia = null; // Completar
+        List<Producto> listaLimpia = new ArrayList<>(productosUnicos);
+
 
         // TODO: TAREA 3 - Filtrar y Transformar
         // Quedarse solo con los de precioBase > 300 y subirles el precio un 21% (IVA).
+        List <Producto> listaFiltrada = new ArrayList<>();
+
+        for (Producto producto : listaLimpia){
+            if (producto.getPrecioBase() > 300){
+                double precioConIVA = producto.getPrecioBase() * 1.21;
+                producto.setPrecioBase(precioConIVA);
+                listaFiltrada.add(producto);
+            }
+        }
+        listaLimpia = listaFiltrada;
+
 
         // TODO: TAREA 4 - Ordenar por Stock de mayor a menor
         // Podéis usar lista.sort() con un Comparator[cite: 206].
+        listaLimpia.sort((p1, p2) -> p2.getStock() - p1.getStock());
 
         System.out.println("\n--- INVENTARIO FINAL (Precio > 300€, con IVA, ordenado por Stock) ---");
         // Imprimir el resultado final
+        for (Producto p : listaLimpia) {
+            System.out.println(p);
+        }
     }
 }
--- src/ejerciciosCasa/e05SetStockElectroAvila/Producto.java
+++ src/ejerciciosCasa/e05SetStockElectroAvila/Producto.java
@@ -1,4 +1,9 @@
+package ejerciciosCasa.e05SetStockElectroAvila;
+
+import java.util.Objects;
+
 public class Producto {
+
     private String id;
     private String modelo;
     private double precioBase;
@@ -26,4 +31,30 @@ public class Producto {
     // TODO: TAREA DEL ALUMNO
     // Implementar equals y hashCode basándose ÚNICAMENTE en el 'id'.
     // Recordad que sin esto, el Set no detectará duplicados.
+
+
+    @Override
+    public boolean equals(Object obj) {
+        // Compara si variable actual y variable que entra por parametro apuntan al misma direcc son mismo objeto en memoria
+        if (this == obj) {
+            return true;
+        }
+        // Primero comparar si no existe / Segundo compara que la naturaleza de objetos (un producto y un clientes no pueden ser iguales)
+        if (obj == null || getClass() != obj.getClass()){
+            return false;
+        }
+
+        // Se hace un downCast, para pasar de objeto a producto concreto
+        Producto producto = (Producto) obj;
+
+        //Compara si es lo mismo
+        return Objects.equals(this.id,producto.id);
+
+    }
+
+    @Override
+    public int hashCode() {
+        // Genera del hash basado exclusivamente en el ID
+        return Objects.hash(this.id);
+    }
 }
