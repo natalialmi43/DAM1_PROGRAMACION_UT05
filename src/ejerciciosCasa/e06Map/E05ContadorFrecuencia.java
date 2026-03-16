fd9ea9451a0f956fae232cea4c6f48a089ee403d
MCN <natalialmi43@gmail.com>
2026-03-16 17:13:21 +0100

    WIP

---
+++ src/ejerciciosCasa/e06Map/E05ContadorFrecuencia.java
@@ -0,0 +1,10 @@
+package ejerciciosCasa.e06Map;
+
+public class E05ContadorFrecuencia {
+
+    public static void main(String[] args) {
+
+        String [] lenguajes = {"Java", "Python", "Java", "C++", "Java", "Python"};
+
+    }
+}
