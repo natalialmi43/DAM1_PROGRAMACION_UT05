af1d2b24b33719a754619d0719cefb2a5b6a99ce
MCN <natalialmi43@gmail.com>
2026-03-18 19:48:35 +0100

    WIP

---
+++ src/ejerciciosCasa/e05SetStockElectroAvila/SOS.java
@@ -0,0 +1,38 @@
+package ejerciciosCasa.e05SetStockElectroAvila;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SOS {
+
+    public static void main(String[] args) {
+
+        Map<String, Map<String, Integer>> ventasPorZona = new HashMap<>();
+        ventasPorZona.putIfAbsent("Avila", new HashMap<>());
+        ventasPorZona.putIfAbsent("Burgos", new HashMap<>());
+        ventasPorZona.putIfAbsent("Segovia", new HashMap<>());
+        ventasPorZona.get("Avila").put("Luis", 4500);
+        ventasPorZona.get("Avila").put("Fernando", 3200);
+        ventasPorZona.get("Avila").put("Marta", 7000);
+        ventasPorZona.get("Segovia").put("Rigoberto", 6200);
+        ventasPorZona.get("Segovia").put("Luciano", 5200);
+        ventasPorZona.get("Segovia").put("Marta", 1200);
+        ventasPorZona.get("Burgos").put("Fernando", 4500);
+        ventasPorZona.get("Burgos").put("Rigoberto", 7200);
+
+        // Transformar en ventas por vendedor
+        // Luis > Avila > 4500
+        // Fernando > Avila > 3200
+        // Fernando > Burgos > 4500
+        Map<String, Map<String, Integer>> ventasPorVendedor = new HashMap<>();
+
+        for (String ventasPorCiudad: ventasPorZona.keySet()){
+            System.out.println(ventasPorCiudad);
+            for (String vendedor : ventasPorZona.)
+        }
+
+
+
+
+    }
+}
